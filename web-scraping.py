b257c8f93449a71f6a496f4c79b1e6b293df1cc8
bazza-ctrl <avanegas095@gmail.com>
2022-02-17 21:40:09 -0500

    Updated

--- web-scraping.py
+++ web-scraping.py
@@ -5,26 +5,19 @@ from bs4 import BeautifulSoup
 
 def getInfo(url):
     try:
-        html = urllib.request.urlopen(url)
+        html = urlopen(url)
     except HTTPError as e:
         return None # Return a null, break,... url.
     try: # If the tag we're trying to get in does not exis
-        obj = BeautifulSoup(url.read())
-        infoFrom = obj.body.div.a  class="btn btn-primary btn-sm view-data" href="/Salud-y-Protecci-n-Social/Casos-positivos-de-COVID-19-en-Colombia/gt2j-8ykr/data"
+        obj = BeautifulSoup(html.read())
+        infoFrom = obj.main.h1
     except AttributeError as e:
         return None
     return infoFrom
-
-infoFrom = getTitle("https://www.datos.gov.co/Salud-y-Protecci-n-Social/Casos-positivos-de-COVID-19-en-Colombia/gt2j-8ykr")
-
+infoFrom = getInfo("https://www.datos.gov.co/Salud-y-Protecci-n-Social/Casos-positivos-de-COVID-19-en-Colombia/gt2j-8ykr")
 if infoFrom == None:
     print("Title could not be found")  
-    else:
+else:
         print(infoFrom)
-
-    
-
-
-
 
 
