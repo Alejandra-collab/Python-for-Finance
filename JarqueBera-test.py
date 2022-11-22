7ec64f7d0526502c3f7bd9c56ddd830552a0f5a9
Alejandra-collab <anvanegasg@unal.edu.co>
2022-11-22 16:44:14 -0500

    Updated

--- JarqueBera-test.py
+++ JarqueBera-test.py
@@ -1,6 +1,3 @@
-# Objetivo: Distribuciones en Python: (i) Calculo de funciones estadísticas, momentos, percentiles y Valor en Riesgo (VaR); (ii) Visualización de datos en Python: histogramas; (iii) Test de normalidad o Gaussianidad: Jarque-Bera; y (iv) Los limites del p-value y los test estadísticos: cuando una distribución falla el test de normalidad.
-
-
 import numpy as np
 import pandas as pd
 import matplotlib as mpl
@@ -9,14 +6,14 @@ import importlib
 import matplotlib.pyplot as plt
 from scipy.stats import skew, kurtosis, chi2 #_contingency
 
+# ----------------------------- 1.DISTRIBUCIONES -------------------------
 
 # Queremos hacer un test de Python que me calcule un test de normalidad de rendimientos. Primero hacerlo con rendimientos simulados.
+# Hay varios test de normalidad, uno de ellos se llama de Jerque-bera:
 
-# Hay varios test de normalidad. Uno se llama de Jerque-bera
-
-# Paso 1: Generar variables aleatorias
-
+# Definimos el tamaño de la muestra para empezar a trabajar con los datos
 x_size = 10**6
+
 degrees_freedom = 5 # Si los degrees_freedom de una student tienden a infinito, la distribución se parecerá mucho a una normal
 
 # Aquí solo tenemos que cambiar el tipo de distribución de type_random_variable según necesitemos:
@@ -38,31 +35,80 @@ elif type_random_variable == 'chi-square':
     #Para que se muestren los grados de libertad, df, tenemos que cambiar los degrees_freedom de entero a string. Eso lo hacemos con str() 
     x_str = type_random_variable + ' (df=' + str(degrees_freedom) + ')'
 
-
-# Compute "risk metrics"
+# 1.1. Cálculo de funciones estadísticas ---------------------------------
 x_mean = np.mean(x)
 x_stdev = np.std(x)
 x_skew = skew(x)
 x_kurt = kurtosis(x) # Esta es kurtosis en exceso
 x_var_95 = np.percentile(x, 5) #var: valor en riesgo 
+
+# 1.2. Test de Jarque Bera
 x_jb = x_size/6 * (x_skew**2 + 1/4 * x_kurt**2)
 p_value = 1 - chi2.cdf(x_jb, df = degrees_freedom) 
 is_normal = (p_value > 0.05) # Rechazamos la hipótesis cuando el p_value sea menor que el nivel de significancia
 
 
-# Print metrics
-print('mean' + str(x_mean))
-print('std' + str(x_stdev))
-print('skewness' + str(x_skew)) #Si es >media, los datos están cargados a la derecha de la distribución. <media significa que la distribución está cargada a la izquierda de la distribución
-print('kurtosis' + str(x_kurt)) #Si la kurtosis <0, la variable aleatoria con la que estoy jugando decrece más rápido que la normal. Si k_3 (kurtosisen exceso) > 0 decrece menos rápido que la normal. k_3 = 0 significa que es una normal
-print('VaR 95%' + str(x_var_95)) 
-print('Jarque-Bera' + str(x_jb))
-print('p-value' + str(p_value))
+# Resultados:
+print('mean: ' + str(x_mean))
+print('std: ' + str(x_stdev))
+print('skewness: ' + str(x_skew)) #Si es >media, los datos están cargados a la derecha de la distribución. <media significa que la distribución está cargada a la izquierda de la distribución
+print('kurtosis: ' + str(x_kurt)) #Si la kurtosis <0, la variable aleatoria con la que estoy jugando decrece más rápido que la normal. Si k_3 (kurtosisen exceso) > 0 decrece menos rápido que la normal. k_3 = 0 significa que es una normal
+print('VaR 95%: ' + str(x_var_95)) 
+print('Jarque-Bera: ' + str(x_jb))
+print('p-value: ' + str(p_value))
 
-# Paso 2: Visualizar histograma
+# 1.3. Visualización de datos --------------------------------------------
 
 plt.figure()
 plt.hist(x, bins = 100)
 plt.title('Histogram ' + x_str)
 plt.show()
+
+
+# 1.4. Teorema de Borel-Cantelli -----------------------------------------
+
+# Usar un nivel de confianza del 95%, por ejemplo, significa que aún a un nivel de confianza alto y corriendo el programa infinidad de veces, habrá alguna distribución normal cuyo test de Jarque Bera la haga ver como no normal. Esto es el llamado teorema de Borel-Cantelli. Veamos:
+
+# Cuando el conjunto de datos falle el test de normalidad el programa se va a detener:
+is_normal = True
+counter = 0
+while is_normal:
+
+    x_size = 10**6
+    degrees_freedom = 5000
+
+    type_random_variable = 'normal'
+    if type_random_variable == 'normal':
+        x = np.random.standard_normal(x_size)
+        x_str = type_random_variable
+    elif type_random_variable == 'exponential':
+        x = np.random.standard_exponential(x_size)
+        x_str = type_random_variable
+    elif type_random_variable == 'student':
+        x = np.random.standard_t(df = degrees_freedom, size = x_size)
+        x_str = type_random_variable
+    elif type_random_variable == 'chi-square':
+        x = np.random.chisquare(df = degrees_freedom, size = x_size)
+        x_str = type_random_variable
+
+        x_str = type_random_variable + ' (df=' + str(degrees_freedom) + ')'
+
+
+    # Test de normalidad -------------------------------------------------
+    x_skew = skew(x)
+    x_kurt = kurtosis(x)
+    x_jb = x_size/6 * (x_skew**2 + 1/4 * x_kurt**2)
+    p_value = 1 - chi2.cdf(x_jb, df = degrees_freedom)
+
+    # Visualizar el cálculo de las funciones -----------------------------
+    print('skewness' + str(x_skew))
+    print('kurtosis' + str(x_kurt))
+    print('Jarque-Bera' + str(x_jb))
+    print('p-value' + str(p_value))
+
+    # El contador se detendrá cuando un conjunto de datos no pase el test de normalidad
+    print('counter' + str(counter))
+    counter +=1
+    print('-----------------')
+
 
--- README.md
+++ README.md
@@ -7,8 +7,16 @@ In this script you are going to be able to look the code to download data from Y
 In this script I expose how to use Python to request information from a web page in an automated way.
 
 ### JarqueBera-test
+#### Temario:
+1. Distribuciones
+1.1. Calculo de funciones estadísticas
+1.2. Test de Jarque Bera
+1.3. Visualización de datos
+1.4. Teorema de Borel Cantelli
 The Jarque-Bera test is a goodness of fit test (which describes how well a set of observations fit).
 
 If the data comes from a normal distribution, the JB test will behave like a chi-square distribution with two degrees of freedom.
 
-The null hypothesis is that our distribution is normal (skewness = 0, excess kurtosis = 0). With a JB > 6 the distribution is not normal.
+The null hypothesis means our distribution is normal (skewness = 0, excess kurtosis = 0). With a JB > 6 the distribution is not normal.
+
+#### Objetivo: Distribuciones en Python: (i) Calculo de funciones estadísticas, momentos, percentiles y Valor en Riesgo (VaR); (ii) Visualización de datos en Python: histogramas; (iii) Test de normalidad o Gaussianidad: Jarque-Bera; y (iv) Los limites de p-value y los test estadísticos: teorema de Borel-Cantelli
